95f0e4ff970ba232814549a585734f99d2d3e15a
Jekplex <41015593+Jekplex@users.noreply.github.com>
2025-06-27 00:13:23 +0100

    early days

--- ui/src/app/app.ts
+++ ui/src/app/app.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'starwars-explorer';
+
+  private isReleaseView : boolean = false;
+  
+  setReleaseView(bool: boolean): void {
+    this.isReleaseView = bool;
+  }
+  
+  getReleaseView(): boolean {
+    // console.log(this.isReleaseView);
+    return this.isReleaseView;
+  }
+
 }
